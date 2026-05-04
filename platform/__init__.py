d8461744c5f9066fe6d6589a48a2d2bfb14b5c8c
wolandvp-beep <woland.vp@gmail.com>
2026-05-04 11:30:00 +1000

    Delete platform directory

--- platform/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from .package_bootstrap import bootstrap_package
-
-from .compat_paths import PLATFORM_COMPAT_DIRS, PLATFORM_ROOT, extend_module_path
-
-__path__ = bootstrap_package(__path__, __name__, PLATFORM_COMPAT_DIRS)
-
-__all__ = ['PLATFORM_COMPAT_DIRS', 'PLATFORM_ROOT']
--- platform/runtime_assets/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from ..package_bootstrap import bootstrap_package
-from ..layout_specs.shared import RUNTIME_ASSETS_ROOT
-
-__path__ = bootstrap_package(__path__, __name__, [RUNTIME_ASSETS_ROOT])
-
-__all__ = ['RUNTIME_ASSETS_ROOT']
