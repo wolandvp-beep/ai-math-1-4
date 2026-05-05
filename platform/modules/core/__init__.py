5a315f962cfa560a845f814e74413ccc624f2440
wolandvp-beep <woland.vp@gmail.com>
2026-05-06 09:51:08 +1000

    Delete platform directory

--- platform/modules/core/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from backend.package_bootstrap import bootstrap_package
-from backend.layout_specs.core import CORE_ROOT
-
-__path__ = bootstrap_package(__path__, __name__, [CORE_ROOT])
-
-__all__ = ['CORE_ROOT']
