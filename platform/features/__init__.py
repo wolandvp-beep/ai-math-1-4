3bb47fdba495409e0ea063df597a28d4bd21fd72
wolandvp-beep <woland.vp@gmail.com>
2026-05-21 11:39:16 +1000

    -

--- platform/features/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from ..package_bootstrap import bootstrap_package
-from ..layout_specs.shared import FEATURES_ROOT
-
-__path__ = bootstrap_package(__path__, __name__, [FEATURES_ROOT])
-
-__all__ = ['FEATURES_ROOT']
