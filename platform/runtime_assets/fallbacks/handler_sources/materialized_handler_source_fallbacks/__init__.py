ed6c3492044c6ceae1f2ec5b1e6308a50ed87dde
wolandvp-beep <woland.vp@gmail.com>
2026-05-22 08:58:29 +1000

    -

--- platform/runtime_assets/fallbacks/handler_sources/materialized_handler_source_fallbacks/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from backend.package_bootstrap import bootstrap_package
-
-from backend.platform.compat_paths import MATERIALIZED_HANDLER_SOURCE_FALLBACK_IMPORT_PACKAGE_DIRS, extend_module_path
-
-__path__ = bootstrap_package(__path__, __name__, MATERIALIZED_HANDLER_SOURCE_FALLBACK_IMPORT_PACKAGE_DIRS)
-
-__all__ = ['MATERIALIZED_HANDLER_SOURCE_FALLBACK_IMPORT_PACKAGE_DIRS']
