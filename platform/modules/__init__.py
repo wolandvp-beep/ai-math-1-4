55b9af196b74ff53afc3a5c84065022f70005731
wolandvp-beep <woland.vp@gmail.com>
2026-05-25 09:26:57 +1000

    -

--- platform/modules/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from ..package_bootstrap import bootstrap_package
-from ..layout_specs.shared import MODULES_ROOT
-
-__path__ = bootstrap_package(__path__, __name__, [MODULES_ROOT])
-
-__all__ = ['MODULES_ROOT']
