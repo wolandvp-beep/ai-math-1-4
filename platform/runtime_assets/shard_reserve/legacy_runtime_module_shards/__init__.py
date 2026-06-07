829f99f445a76e5b1801d28769d66c4ca1159576
wolandvp-beep <woland.vp@gmail.com>
2026-06-08 08:47:35 +1000

    -

--- platform/runtime_assets/shard_reserve/legacy_runtime_module_shards/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from backend.package_bootstrap import bootstrap_package
-from backend.layout_specs.runtime_assets import LEGACY_RUNTIME_MODULE_SHARDS_ROOT
-
-__path__ = bootstrap_package(__path__, __name__, [LEGACY_RUNTIME_MODULE_SHARDS_ROOT])
-
-__all__ = ['LEGACY_RUNTIME_MODULE_SHARDS_ROOT']
