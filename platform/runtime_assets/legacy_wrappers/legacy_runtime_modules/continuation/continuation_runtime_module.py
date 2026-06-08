648515bb5d5ceee50ddff5506748f820a546fdc7
wolandvp-beep <woland.vp@gmail.com>
2026-06-08 16:51:15 +1000

    Add files via upload

---
+++ platform/runtime_assets/legacy_wrappers/legacy_runtime_modules/continuation/continuation_runtime_module.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from backend.runtime_assets.wrapper_bootstrap import init_runtime_module_wrapper
+
+
+__all__, __wrapper_origin__ = init_runtime_module_wrapper(globals())
