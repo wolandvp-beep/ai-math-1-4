6c0479c22e5c50daf37e22f17b0ec37cecc67194
wolandvp-beep <woland.vp@gmail.com>
2026-06-10 16:36:15 +1000

    -

--- platform/runtime_assets/legacy_wrappers/legacy_runtime_sources/build/prepatch_build_source.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from backend.runtime_assets.wrapper_bootstrap import init_runtime_source_wrapper
-
-
-__all__, __wrapper_origin__ = init_runtime_source_wrapper(globals())
