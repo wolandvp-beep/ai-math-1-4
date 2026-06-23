37634f88dce97191b4bae60b9617f05978709e95
wolandvp-beep <wolandvp-beep@users.noreply.github.com>
2026-06-23 10:36:41 +1000

    Deploy merged_v525_01_v50103_excel_1601_1700

--- backend/service.py
+++ backend/service.py
@@ -12,8 +12,8 @@ from backend.text_utils import NON_MATH_REPLY, looks_like_math_input
 from backend.platform.request_shape_guards import build_multi_task_payload, canonicalize_system_submission, is_multi_task_submission
 from backend.live_math_solver import solve_live_math_first
 
-APP_RELEASE = 'v524_01_v50103_excel_1501_1600'
-SOLVER_VERSION = 'v524-01-v50103-excel-1501-1600'
+APP_RELEASE = 'v525_01_v50103_excel_1601_1700'
+SOLVER_VERSION = 'v525-01-v50103-excel-1601-1700'
 
 _BAD_INTERNAL_MARKERS = (
     'Zad3',
